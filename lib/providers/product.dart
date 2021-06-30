446d7c0758f0cc403aacc31d33394825489371c1
Saransh <mittalsaransh2000@gmail.com>
2021-06-30 21:58:54 +0530

    first commit

---
+++ lib/providers/product.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+
+class Product with ChangeNotifier {
+  final String id;
+  final String title;
+  final String description;
+  final double price;
+  final String imageUrl;
+  bool isFavorite;
+
+  Product({
+    @required this.id,
+    @required this.title,
+    @required this.description,
+    @required this.price,
+    @required this.imageUrl,
+    this.isFavorite = false,
+  });
+
+  void _setFavValue(bool newValue) {
+    isFavorite = newValue;
+    notifyListeners();
+  }
+
+  Future<void> toggleFavoriteStatus(String token, String userId) async {
+    final oldStatus = isFavorite;
+    final url =
+        'https://shop-app-3247b-default-rtdb.firebaseio.com/userFavorites/$userId/$id.json?auth=$token';
+    isFavorite = !isFavorite;
+    notifyListeners();
+    try {
+      final response = await http.put(
+        Uri.parse(url),
+        body: json.encode(
+          isFavorite,
+        ),
+      );
+      if (response.statusCode >= 400) {
+        _setFavValue(oldStatus);
+      }
+    } catch (error) {
+      isFavorite = oldStatus;
+      _setFavValue(oldStatus);
+    }
+  }
+}
